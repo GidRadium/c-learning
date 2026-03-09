bf0db1c8321f30bdf1728fc60ef6eb25b1448679
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-09 17:09:42 +0300

    Add CsvRow struct

--- Task-01-CSVPP/CsvParser.c
+++ Task-01-CSVPP/CsvParser.c
@@ -1,4 +1,24 @@
 #include "CsvParser.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_LINE_LENGTH 4000
+
+typedef struct CsvElementInRow {
+    size_t startIndexInRow;
+    size_t size;
+    bool isNumber;
+} CsvElementInRow;
+
+typedef struct CsvRow {
+    char *line;
+    size_t elementsCount;
+    CsvElementInRow *elements;
+
+} CsvRow;
+
+CsvRow* getRow();
+void deleteRow(CsvRow *row);
 
 typedef struct CsvParser CsvParser;
 
