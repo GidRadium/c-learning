be2d891da8e1c3509a285781388351950ea1fd03
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-09 21:27:56 +0300

    Add comments.

--- Task-01-CSVPP/CsvParser.h
+++ Task-01-CSVPP/CsvParser.h
@@ -2,10 +2,14 @@
 
 #include <stdio.h>
 
+// Represents .csv file as .txt table.
 typedef struct CsvParser CsvParser;
 
+// Reads data from .csv to the memmory.
 CsvParser* createFromFile(FILE *file);
 
+// Writes table to text file.
 void writeToFile(CsvParser *parser, FILE *file);
 
+// Frees all allocated memmory.
 void deleteCsvParser(CsvParser *parser);
