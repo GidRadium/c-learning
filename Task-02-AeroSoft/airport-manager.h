1a41b5e9fb9c49e9b2ba557ec8a0d4c41a3cab8e
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-21 22:00:32 +0300

    Add airport-manager.h

---
+++ Task-02-AeroSoft/airport-manager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stddef.h>
+#include "map.h"
+
+typedef enum {
+    AmSucsess,
+    AmErrNoManager,
+    AmErrNoFile,
+    AmErrOnMalloc,
+    AmErrIataCodeIncorrect,
+    AmErrAirportAlreadyExist,
+    AmErrAirportNotFound,
+    AmErrNameTooLong,
+
+} AirportManagerReturnCode;
+
+typedef struct AirportManager AirportManager;
+
+// AmSucsess, AmErrOnMalloc
+AirportManagerReturnCode amCreateManager(AirportManager** manager);
+
+// AmSucsess, AmErrNoManager, AmErrNoFile, AmErrOnMalloc
+AirportManagerReturnCode amInitFromFile(AirportManager* manager, const char* path, size_t* airportsNumber);
+
+// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound, AmErrNameTooLong
+AirportManagerReturnCode amFindAirport(AirportManager* manager, const char* iataCode, char* nameBuffer, size_t bufferSize);
+
+// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportAlreadyExist, AmErrOnMalloc
+AirportManagerReturnCode amAddAirport(AirportManager* manager, const char* iataCode, const char* name);
+
+// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound
+AirportManagerReturnCode amDeleteAirport(AirportManager* manager, const char* iataCode);
+
+// AmSucsess, AmErrNoManager, AmErrNoFile
+AirportManagerReturnCode amSaveToFile(AirportManager* manager, const char* path, size_t* airportsNumber);
+
+// AmSucsess, AmErrNoManager
+AirportManagerReturnCode amDeleteManager(AirportManager *manager);
