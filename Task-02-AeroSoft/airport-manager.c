dfad7048d500ccdb77c8f61913809cdfa835389e
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-27 17:22:25 +0300

    Add comments and funcs to airport-manager

---
+++ Task-02-AeroSoft/airport-manager.c
@@ -0,0 +1,80 @@
+#include "airport-manager.h"
+
+#include "map.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+MapValue copyString(MapValue value)
+{
+    if (value == NULL) {
+        return NULL;
+    }
+
+    return strdup((char*)value);
+}
+
+void freeString(MapValue value)
+{
+    if (value == NULL) {
+        return;
+    }
+
+    free(value);
+}
+
+bool isIataCorrect(const char* iataCode)
+{
+    if (!iataCode || strlen(iataCode) != 3) return false;
+
+    return (isupper(iataCode[0]) && isupper(iataCode[1]) && isupper(iataCode[2]));
+}
+
+MapKey iataToKey(const char* code)
+{
+    return (code[0] << 16) | (code[1] << 8) | code[2];
+}
+
+
+typedef struct AirportManager {
+    Map* data;
+
+} AirportManager;
+
+
+AirportManagerReturnCode amCreateManager(AirportManager** manager)
+{
+
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amInitFromFile(AirportManager* manager, const char* path, size_t* airportsNumber)
+{
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amFindAirport(AirportManager* manager, const char* iataCode, char* nameBuffer, size_t bufferSize)
+{
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amAddAirport(AirportManager* manager, const char* iataCode, const char* name)
+{
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amDeleteAirport(AirportManager* manager, const char* iataCode)
+{
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amSaveToFile(AirportManager* manager, const char* path, size_t* airportsNumber)
+{
+    return AmSucsess;
+}
+
+AirportManagerReturnCode amDeleteManager(AirportManager** manager)
+{
+    return AmSucsess;
+}
--- Task-02-AeroSoft/airport-manager.h
+++ Task-02-AeroSoft/airport-manager.h
@@ -16,23 +16,30 @@ typedef enum {
 
 typedef struct AirportManager AirportManager;
 
-// AmSucsess, AmErrOnMalloc
+// Allocates memmory for manager struct. Manager is NULL if something went wrong.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrOnMalloc
 AirportManagerReturnCode amCreateManager(AirportManager** manager);
 
-// AmSucsess, AmErrNoManager, AmErrNoFile, AmErrOnMalloc
+// Opens file and reads table in IATA:NAME format. Closes file. Allocates memmory for all data.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrNoFile, AmErrOnMalloc
 AirportManagerReturnCode amInitFromFile(AirportManager* manager, const char* path, size_t* airportsNumber);
 
-// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound, AmErrNameTooLong
+// Copy name of airport to buffer by it IATA code. Otherwise do nothing with buffer.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound, AmErrNameTooLong
 AirportManagerReturnCode amFindAirport(AirportManager* manager, const char* iataCode, char* nameBuffer, size_t bufferSize);
 
-// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportAlreadyExist, AmErrOnMalloc
+// Adds airport name by IATA code. Do nothing if already exists. Allocates memmory for it.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportAlreadyExist, AmErrOnMalloc
 AirportManagerReturnCode amAddAirport(AirportManager* manager, const char* iataCode, const char* name);
 
-// AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound
+// Deletes airport by it IATA code. Do nothing if not exists.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrIataCodeIncorrect, AmErrAirportNotFound
 AirportManagerReturnCode amDeleteAirport(AirportManager* manager, const char* iataCode);
 
-// AmSucsess, AmErrNoManager, AmErrNoFile
+// Saves all data to the file in IATA:NAME format. Rewrites it if already exists.
+// Possible return codes: AmSucsess, AmErrNoManager, AmErrNoFile
 AirportManagerReturnCode amSaveToFile(AirportManager* manager, const char* path, size_t* airportsNumber);
 
-// AmSucsess, AmErrNoManager
+// Deletes manager struct. Free all allocated data. Sets it to NULL.
+// Possible return codes: AmSucsess, AmErrNoManager
 AirportManagerReturnCode amDeleteManager(AirportManager** manager);
