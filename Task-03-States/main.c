6093826a673ecf8610eb89e375e4661718e8b809
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-04-24 00:00:39 +0300

    Change output.

--- Task-03-States/main.c
+++ Task-03-States/main.c
@@ -33,9 +33,41 @@ int main()
 
     Partition partition = graphSolve(graph, k, capitals);
 
+    int* stateSizes = calloc(k, sizeof(int));
     for (size_t i = 0; i < partition.size; i++) {
-        printf("%d %d\n", partition.data[i].city, partition.data[i].state);
+        stateSizes[partition.data[i].state]++;
     }
+
+    int** groups = malloc(k * sizeof(int*));
+    for (int i = 0; i < k; i++) {
+        groups[i] = malloc(stateSizes[i] * sizeof(int));
+    }
+
+    int* offsets = calloc(k, sizeof(int));
+    for (size_t i = 0; i < partition.size; i++) {
+        int state = partition.data[i].state;
+        int city = partition.data[i].city;
+
+        groups[state][offsets[state]++] = city;
+    }
+
+    for (int state = 0; state < k; state++) {
+        printf("%d:", state);
+
+        for (int i = 0; i < stateSizes[state]; i++) {
+            printf(" %d", groups[state][i]);
+        }
+
+        printf("\n");
+    }
+
+    for (int i = 0; i < k; i++) {
+        free(groups[i]);
+    }
+
+    free(groups);
+    free(stateSizes);
+    free(offsets);
 
     partitionDelete(&partition);
     graphDelete(&graph);
